ba2301ad8c1956644c887772631a3fd0b29a5247
poopijay <jaymarkmunda26@gmail.com>
2022-07-06 22:33:17 +0800

    add styles and functionality to tabs component

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -5,41 +5,49 @@ import { HiOutlineMail } from "react-icons/hi";
 
 import { messages } from "../../personal-data";
 import profileImg from "../../assets/user-profile.svg";
+import Tabs from "../../components/Tabs/Tabs";
 
 const About = () => {
   return (
-    <section id="about" className="container">
-      {/* <h2 className="about-header">ABOUT ME</h2> */}
-      <article className="about-container text">
-        <h2>Hi there!</h2>
-        {messages.map((message) => {
-          const { id, text } = message;
-          return <p key={id}>{text}</p>;
-        })}
-        <button className="btn">Grab Resume</button>
-      </article>
-      <div className="about-container photo">
-        <img src={profileImg} alt="photo" />
-        {/* <div className="underline" /> */}
-        <div className="social-icons">
-          <button>
-            <a href="mailto:jaymakmunda26@gmail.com">
-              <HiOutlineMail />
-            </a>
-          </button>
-          <button>
-            <a href="https://www.linkedin.com/in/jay-mark-munda-18a208233/">
-              <FaLinkedin />
-            </a>
-          </button>
-          <button>
-            <a href="https://github.com/JmarkMunda">
-              <FaGithub />
-            </a>
-          </button>
+    <div>
+      <section id="about" className="container">
+        {/* <h2 className="about-header">ABOUT ME</h2> */}
+        <div></div>
+        <article className="about-container text">
+          <h2>Hi there!</h2>
+          {messages.map((message) => {
+            const { id, text } = message;
+            return <p key={id}>{text}</p>;
+          })}
+          <button className="btn">Grab Resume</button>
+        </article>
+        <div className="about-container photo">
+          <img src={profileImg} alt="photo" />
+          {/* <div className="underline" /> */}
+          <div className="social-icons">
+            <button>
+              <a href="mailto:jaymakmunda26@gmail.com">
+                <HiOutlineMail />
+              </a>
+            </button>
+            <button>
+              <a href="https://www.linkedin.com/in/jay-mark-munda-18a208233/">
+                <FaLinkedin />
+              </a>
+            </button>
+            <button>
+              <a href="https://github.com/JmarkMunda">
+                <FaGithub />
+              </a>
+            </button>
+          </div>
         </div>
+      </section>
+      {/* Tabs */}
+      <div id="tabs-container" className="container">
+        <Tabs />
       </div>
-    </section>
+    </div>
   );
 };
 
