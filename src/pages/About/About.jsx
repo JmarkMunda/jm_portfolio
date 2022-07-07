4e411a23fb2cf2818fa15634970e09940dcf13db
poopijay <jaymarkmunda26@gmail.com>
2022-07-07 13:12:34 +0800

    restyle components

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -9,45 +9,45 @@ import Tabs from "../../components/Tabs/Tabs";
 
 const About = () => {
   return (
-    <div>
-      <section id="about" className="container">
-        {/* <h2 className="about-header">ABOUT ME</h2> */}
-        <div></div>
-        <article className="about-container text">
-          <h2>Hi there!</h2>
-          {messages.map((message) => {
-            const { id, text } = message;
-            return <p key={id}>{text}</p>;
-          })}
-          <button className="btn">Grab Resume</button>
+    <section id="about">
+      <div className="container">
+        <article className="about-container">
+          {/* ------------- TEXT ----------------- */}
+          <>
+            <div className="text-container">
+              <h2>Hi there!</h2>
+              {messages.map((message) => {
+                const { id, text } = message;
+                return <p key={id}>{text}</p>;
+              })}
+              <button className="btn">Grab Resume</button>
+            </div>
+            <img src={profileImg} alt="photo" />
+            <div className="social-icons">
+              <button>
+                <a href="mailto:jaymakmunda26@gmail.com">
+                  <HiOutlineMail />
+                </a>
+              </button>
+              <button>
+                <a href="https://www.linkedin.com/in/jay-mark-munda-18a208233/">
+                  <FaLinkedin />
+                </a>
+              </button>
+              <button>
+                <a href="https://github.com/JmarkMunda">
+                  <FaGithub />
+                </a>
+              </button>
+            </div>
+          </>
+          {/* ------------- TABS ----------------- */}
+          {/* <div className="container">
+            <Tabs />
+          </div> */}
         </article>
-        <div className="about-container photo">
-          <img src={profileImg} alt="photo" />
-          {/* <div className="underline" /> */}
-          <div className="social-icons">
-            <button>
-              <a href="mailto:jaymakmunda26@gmail.com">
-                <HiOutlineMail />
-              </a>
-            </button>
-            <button>
-              <a href="https://www.linkedin.com/in/jay-mark-munda-18a208233/">
-                <FaLinkedin />
-              </a>
-            </button>
-            <button>
-              <a href="https://github.com/JmarkMunda">
-                <FaGithub />
-              </a>
-            </button>
-          </div>
-        </div>
-      </section>
-      {/* Tabs */}
-      <div id="tabs-container" className="container">
-        <Tabs />
       </div>
-    </div>
+    </section>
   );
 };
 
