0f7bdc0697a8e94721940bf9c1e93fba60eb4b5e
poopijay <jaymarkmunda26@gmail.com>
2022-07-11 14:14:31 +0800

    add projects-data.js

--- src/pages/Projects/Projects.jsx
+++ src/pages/Projects/Projects.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import "./Projects.css";
 import projectImg from "../../assets/code.png";
 import { BsGithub } from "react-icons/bs";
+import { projects } from "../../projects-data";
+import { useRef } from "react";
 
 const Projects = () => {
   return (
@@ -11,42 +13,40 @@ const Projects = () => {
           <h2>Projects</h2>
           <div className="header-line"></div>
         </div>
-        <div className="project">
-          <div className="project-image">
-            <a href="https://jmovies-trailers.netlify.app/" target="_blank">
-              <img src={projectImg} alt="project" />
-            </a>
-          </div>
-          <div className="project-text">
-            <div className="project-title">
-              <p>jMovies</p>
-              {/* Github icon */}
-              <a href="https://github.com/JmarkMunda/jMovies" target="_blank">
-                <span className="github-icon">
-                  <BsGithub />
-                </span>
-                View Code
+        {/* ======= Projects ========= */}
+        {projects.map((project) => {
+          const { id, title, description, image, link, githubLink, libraries } =
+            project;
+          return (
+            <div key={id} className="project">
+              <a href={link} target="_blank" className="project-image">
+                <img src={image} alt="project" />
+                <div className="project-cursor">View Project</div>
               </a>
+              <div className="project-text">
+                <div className="project-title">
+                  <p>{title}</p>
+                  {/* Github icon */}
+                  <a href={githubLink} target="_blank">
+                    <span className="github-icon">
+                      <BsGithub />
+                    </span>
+                    View Code
+                  </a>
+                </div>
+                <p className="project-description">{description}</p>
+                <div className="underline"></div>
+                <div className="project-libraries">
+                  <ul>
+                    {libraries.map((item) => {
+                      return <li>{item}</li>;
+                    })}
+                  </ul>
+                </div>
+              </div>
             </div>
-            <p className="project-description">
-              This application provides you with information about different
-              movies. You can browse movies by searching them manually or by
-              going to the latest, trending, and upcoming movies section.
-            </p>
-            <div className="underline"></div>
-            <div className="project-libraries">
-              <ul>
-                <li>React JS</li>
-                <li>react-router</li>
-                <li>react-youtube</li>
-                <li>TMDB API</li>
-                <li>Swiper JS</li>
-                <li>Framer Motion</li>
-                <li>Axios</li>
-              </ul>
-            </div>
-          </div>
-        </div>
+          );
+        })}
       </div>
     </section>
   );
---
+++ src/projects-data.js
@@ -0,0 +1,25 @@
+
+export const projects = [
+    {
+        id: 1,
+        title: 'jMovies',
+        description: 'This application provides you trailers with information of different movies. You can browse movies by searching them manually or by going to the latest, trending, and upcoming movies section.',
+        image: '/jmovies2.png',
+        link: "https://jmovies-trailers.netlify.app/",
+        githubLink: "https://github.com/JmarkMunda/jMovies",
+        libraries: [
+            "react js", "react-router", "react-youtube", "tmdb api", "swiper js", "framer motion", "axios"
+        ]
+    },
+    {
+        id: 2,
+        title: 'jMovies',
+        description: 'This application provides you trailers with information of different movies. You can browse movies by searching them manually or by going to the latest, trending, and upcoming movies section.',
+        image: '/jmovies2.png',
+        link: "https://jmovies-trailers.netlify.app/",
+        githubLink: "https://github.com/JmarkMunda/jMovies",
+        libraries: [
+            "react js", "react-router", "react-youtube", "tmdb api", "swiper js", "framer motion", "axios"
+        ]
+    }
+]
